e9471458a56f2a5ccd1e8852306ee9676fa1e9ce
Iris397 <55863536+Iris397@users.noreply.github.com>
2019-10-07 14:20:27 -0700

    Update apache-automation.sh

--- apache-automation.sh
+++ apache-automation.sh
@@ -1,5 +1,5 @@
 yum -y install httpd mod-ssl                                                          # Install apache and SSL support
 systemctl start httpd                                                                 # start apache
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                       # comment out the default welcome page
-echo "<html><body><h1>Hi there NTI 300</h1><body></ht,l>" >/var/www/html/index.html   # create custom welcome page
+echo "<html><body><h1>Hi there NTI 300</h1><body></html>" >/var/www/html/index.html   # create custom welcome page
 systemctl restart httpd                                                               # restart apache so our changes take effect
