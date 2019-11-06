9e94a731f5b008de1680d0fc4ba61570ff00e189
Iris397 <55863536+Iris397@users.noreply.github.com>
2019-11-06 15:47:34 -0800

    Update bashshell.sh

--- bashshell.sh
+++ bashshell.sh
@@ -1,6 +1,6 @@
-pushd ()                                          #the first line merely save the first argument in the variable dirname for readability reasons 
-{                                                 #the second line of the function pushs the new directory onto the stack
-    dirname=$1                                    #the third line's main purpose is to change to the new directory
+pushd ()                                          #This line is for save the first argument in the variable dirname for readability reasons 
+{                                                 #It is for push the new directory onto the stack
+    dirname=$1                                    #The reason for third line is to change to the new directory
     DIR_STACK="$dirname ${DIR_STACK:-$PWD' '}"   
     cd ${dirname:?"missing directory name."}
     echo "$DIR_STACK"
@@ -10,7 +10,7 @@ pushd ()                                          #the first line merely save th
     DIR_STACK=${DIR_STACK#* }
     cd ${DIR_STACK%% *}
     echo "$PWD"
-  }          #the last line merely prints the contents of the stack, with the implication that the leftmost directory is both the current directory and at the top of the stack
+  }                                                #This line prints the contents of the stack, with the implication that the leftmost directory is all the current directory and also the top of the stack
 cut -f4 -d\| ablums
 cut -f$(getfield $fieldname) -d\| albums
  who | cut -d' ' -f1
